4bcd734dac7cffa8c12340a56f46121747de751d
Dane Carstens <dcarsten@uci.edu>
2020-10-31 17:34:55 -0700

    Made a scene, added a map prefab, made the camera follow the player

---
+++ Platformer/Assets/Scripts/cameraMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMover : MonoBehaviour
+{
+    public float speed = 1f;
+    private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float interpolation = speed * Time.deltaTime;
+
+        Vector3 position = this.transform.position;
+        position.y = Mathf.Lerp(this.transform.position.y, player.transform.position.y, interpolation);
+        position.x = Mathf.Lerp(this.transform.position.x, player.transform.position.x, interpolation);
+
+        this.transform.position = position;
+    }
+}
